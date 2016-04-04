2094efa91ed6b4402675d3c782b1b145776b1249
Admin VTE <Admin VTE>
2016-04-04 12:18:14 +0200

    First commit

---
+++ lib/JobeetJob.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Jobeet
+{
+    static public function slugify($text)
+    {
+        // replace all non letters or digits by -
+        $text = preg_replace('/\W+/', '-', $text);
+
+        // trim and lowercase
+        $text = strtolower(trim($text, '-'));
+
+        return $text;
+    }
+}
