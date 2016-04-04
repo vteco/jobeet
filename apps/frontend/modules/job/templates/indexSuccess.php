e626af91d5389fd4dfa4e7e0f289c7dff0aec002
Vincent Joly <vincent.joly@primesenergie.fr>
2016-04-04 16:07:42 +0200

    Refactoring on Job Table

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -1,59 +1,25 @@
 <?php use_stylesheet('jobs.css') ?>
-<h1>Jobeet jobs List</h1>
 
-<table>
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Category</th>
-      <th>Type</th>
-      <th>Company</th>
-      <th>Logo</th>
-      <th>Url</th>
-      <th>Position</th>
-      <th>Location</th>
-      <th>Description</th>
-      <th>How to apply</th>
-      <th>Token</th>
-      <th>Is public</th>
-      <th>Is activated</th>
-      <th>Email</th>
-      <th>Expires at</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($jobeet_jobs as $jobeet_job): ?>
-    <tr>
-      <td><a href="<?php echo url_for(array(
-  'module'   => 'job',
-  'action'   => 'show',
-  'id'       => $jobeet_job->getId(),
-  'company'  => $jobeet_job->getCompany(),
-  'location' => $jobeet_job->getLocation(),
-  'position' => $jobeet_job->getPosition(),
-)) ?>"><?php echo $jobeet_job->getId() ?></a></td>
-      
-      <td><?php echo $jobeet_job->getCategoryId() ?></td>
-      <td><?php echo $jobeet_job->getType() ?></td>
-      <td><?php echo $jobeet_job->getCompany() ?></td>
-      <td><?php echo $jobeet_job->getLogo() ?></td>
-      <td><?php echo $jobeet_job->getUrl() ?></td>
-      <td><?php echo $jobeet_job->getPosition() ?></td>
-      <td><?php echo $jobeet_job->getLocation() ?></td>
-      <td><?php echo $jobeet_job->getDescription() ?></td>
-      <td><?php echo $jobeet_job->getHowToApply() ?></td>
-      <td><?php echo $jobeet_job->getToken() ?></td>
-      <td><?php echo $jobeet_job->getIsPublic() ?></td>
-      <td><?php echo $jobeet_job->getIsActivated() ?></td>
-      <td><?php echo $jobeet_job->getEmail() ?></td>
-      <td><?php echo $jobeet_job->getExpiresAt() ?></td>
-      <td><?php echo $jobeet_job->getCreatedAt() ?></td>
-      <td><?php echo $jobeet_job->getUpdatedAt() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-
-  <a href="<?php echo url_for('job/new') ?>">New</a>
+<div id="jobs">
+  <?php foreach ($categories as $category): ?>
+    <div class="category_<?php echo Jobeet::slugify($category->getName()) ?>">
+      <div class="category">
+        <div class="feed">
+          <a href="">Feed</a>
+        </div>
+        <h1>
+          <?php echo link_to($category, 'category', $category) ?>
+        </h1>
+      </div>
+ 
+      <?php include_partial('job/list', array('jobs' => $category->getActiveJobs(sfConfig::get('app_max_jobs_on_homepage')))) ?>
+        
+        <?php if (($count = $category->countActiveJobs() - sfConfig::get('app_max_jobs_on_homepage')) > 0): ?>
+        <div class="more_jobs">
+          and <?php echo link_to($count, 'category', $category) ?>
+          more...
+        </div>
+      <?php endif; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
